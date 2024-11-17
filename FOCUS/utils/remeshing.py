361d5ac8c28867b481fa54bcda08297eec1a2ca8
Ollie Boyne <48369287+OllieBoyne@users.noreply.github.com>
2024-11-17 18:23:32 +0000

    Housekeeping of unused remeshing functions.

--- FOCUS/utils/remeshing.py
+++ FOCUS/utils/remeshing.py
@@ -2,8 +2,6 @@ import trimesh
 import numpy as np
 import pymeshlab  # NOTE: Importing this and open3D together cause some errors on M1 mac with poisson reconstruction.
 import tempfile
-from scipy.spatial import Delaunay
-from time import perf_counter
 import subprocess
 import sys
 
@@ -22,51 +20,9 @@ def _setup_meshlab():
     # TODO: Check whether this is always needed. But setting this to False seems to fix the issue on Macbook M1.
     os.environ['OMP_NUM_THREADS'] = '1'
 
-def add_floor(points):
-    """Add a grid of points within the footprint of the point cloud to force the reconstruction to be watertight."""
-
-    Xp, Yp, Zp = points.T
-
-    N = 100
-    X, Y = np.meshgrid(
-        np.linspace(Xp.min(), Xp.max(), N), np.linspace(Yp.min(), Yp.max(), N)
-    )
-    floor_points = np.stack([X, Y, X * 0], axis=-1).reshape(-1, 3)
-
-    points = np.concatenate([points, floor_points], axis=0)
-    return points
-
-
 def cutoff_above(points, z=0.1) -> (np.ndarray, np.ndarray):
     """Remove all points with a height greater than this z."""
     mask = points[:, 2] < z
-    return points[mask], mask
-
-def cutoff_below(points, z=0.0) -> (np.ndarray, np.ndarray):
-    """Remove all points with a height less than this z."""
-    mask = points[:, 2] > z
-    return points[mask], mask
-
-# def filter_outliers(pts, neighbours=100, std_ratio=0.5):
-#     pcl = o3d.geometry.PointCloud()
-#     pcl.points = o3d.utility.Vector3dVector(pts)
-#     pcl, ind = pcl.remove_statistical_outlier(nb_neighbors=neighbours, std_ratio=std_ratio)
-#     return np.asarray(pcl.points)
-
-
-def pymeshlab_remove_outliers(
-    points: np.ndarray, probability: float = 0.8, n_neighbours: int = 32
-) -> (np.ndarray, np.ndarray):
-    # TODO: switch to Open3D's remove_statistical_outlier - better implementation.
-    mesh = pymeshlab.Mesh(vertex_matrix=points)
-    ms = pymeshlab.MeshSet()
-    ms.add_mesh(mesh)
-
-    ms.compute_selection_point_cloud_outliers(
-        propthreshold=probability, knearest=n_neighbours
-    )
-    mask = np.invert(ms.current_mesh().vertex_selection_array())
-
     return points[mask], mask
 
 
@@ -91,7 +47,6 @@ def pymeshlab_poisson_reconstruct(points, normals=None, depth=8, iters:int = 8, 
         ms.compute_normal_for_point_clouds()
 
     # Threads > 1 seems to cause occasional failures as it is non-deterministic...
-    # TODO: Look at using Quality (confidence = True)
     ms.generate_surface_reconstruction_screened_poisson(
         preclean=True, threads=1, depth=depth, iters=iters, samplespernode=samplespernode,
         cgdepth=cgdepth, pointweight=pointweight, confidence=quality is not None
@@ -99,40 +54,6 @@ def pymeshlab_poisson_reconstruct(points, normals=None, depth=8, iters:int = 8, 
 
     mesh = trimesh.Trimesh(vertices=ms.current_mesh().vertex_matrix(), faces=ms.current_mesh().face_matrix())
 
-    return mesh
-
-
-def poisson_recon_execute(points, normals, depth=6):
-    """Run from PoissonRecon executable."""
-    executable = "/Users/ollie/Documents/repos/phd-projects/FIND2D/misc/PoissonRecon/Bin/Linux/PoissonRecon"
-    with tempfile.NamedTemporaryFile(suffix=".ply") as f:
-        trimesh.Trimesh(vertices=points, vertex_normals=normals).export(f.name)
-        args = [
-            "--in",
-            f.name,
-            "--depth",
-            str(depth),
-            "--out",
-            f.name,
-            "--verbose",
-            "--bType",
-            "3",
-        ]
-
-        subprocess.run([executable] + args, check=True)
-        return trimesh.load(f.name)
-
-
-def delaunay_triangulation(points) -> trimesh.Trimesh:
-    tri = Delaunay(points, furthest_site=True)
-    return trimesh.Trimesh(vertices=points, faces=tri.simplices)
-
-
-def remove_large_faces(mesh, face_area=0.01):
-    """Remove edges that are too long."""
-    face_areas = mesh.area_faces
-    mesh = mesh.copy()
-    mesh.update_faces(face_areas < face_area)
     return mesh
 
 
