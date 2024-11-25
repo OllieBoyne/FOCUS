d32bb5413fa54095431869d50571af08630be898
Ollie Boyne <48369287+OllieBoyne@users.noreply.github.com>
2024-11-25 21:57:42 +0000

    Mesh rendering improvements

--- FOCUS/vis/render_meshes.py
+++ FOCUS/vis/render_meshes.py
@@ -1,11 +1,11 @@
 """Render fitting result from each viewpoint using Blender(synth)"""
 
-import os, sys
+import os
 import blendersynth as bsyn
-from tempfile import mkdtemp
 
 parser = bsyn.ArgumentParser(description="Render fitting result from each viewpoint using Blender(synth)")
 parser.add_argument("--input_directory", type=str, help="Path to the input directory")
+parser.add_argument('--use_color', type=int, default=1, help='1 for use vertex color, 0 otherwise.')
 args = parser.parse_args()
 
 bsyn.run_this_script(open_blender=False, **vars(args))
@@ -25,14 +25,20 @@ p2b = pytoch3d_2_blender_rotmat = mathutils.Matrix([
     [0, 0, -1]
 ])
 
-def main(input_directory):
+def main(input_directory, use_color=True, out_key='mesh_color'):
+    """Render all views, save to output directory.
 
-    bsyn.render.set_cycles_samples(10)
+    :param input_directory: Path to the input directory.
+    :param use_color: If True, use vertex-color if it exists.
+    :param out_key: Output key for the rendered mesh.
+    """
+
+    bsyn.render.set_cycles_samples(2)
 
     # Set up world lighting
     bsyn.world.set_transparent(True)
     bsyn.world.set_color((1.0, 1.0, 1.0))
-    bsyn.world.set_intensity(0.1)
+    bsyn.world.set_intensity(1.0 if use_color else 0.7)
 
     mesh_path = os.path.join(input_directory, 'mesh.obj')
     if not os.path.exists(mesh_path):
@@ -40,6 +46,17 @@ def main(input_directory):
 
     mesh = bsyn.Mesh.from_obj(mesh_path)
     mesh.rotation_euler = (0, 0, 0)
+
+
+    # Default color.
+    mesh.material.shader.inputs['Base Color'].default_value = (0.1, 0.3, 0.85, 1.0)
+
+    # Assign vertex-color if it exists
+    if use_color:
+        if 'Color' in mesh.obj.data.attributes:
+            new_node = mesh.material.nodes.new('ShaderNodeVertexColor')
+            new_node.layer_name = 'Color'
+            mesh.material.links.new(new_node.outputs['Color'], mesh.material.shader.inputs['Base Color'])
 
     # To ensure consistent lighting, we will scale the whole scene to fit the average dimension of the mesh to 1m.
     sf = 1 / np.mean(mesh.dimensions)
@@ -53,11 +70,9 @@ def main(input_directory):
 
     cameras = []
 
-    tmp_output_dir = mkdtemp()
-
     comp = bsyn.Compositor()
 
-    comp.define_output('Image', tmp_output_dir, file_name='mesh')
+    comp.define_output('Image', name='mesh')
 
     for i, view in enumerate(views):
 
@@ -78,22 +93,24 @@ def main(input_directory):
 
         cameras.append(camera)
 
-    #Pick the central most camera, add a spotlight to it.
+    # Spotlight, for non-color only.
+    # Pick the central most camera, add a spotlight to it.
     cameras_centroid = mathutils.Vector(np.mean([c.location for c in cameras], axis=0))
     closest_camera_idx = min(range(len(cameras)), key=lambda i: (cameras[i].location - cameras_centroid).length)
 
-    light = bsyn.Light.create('SPOT', intensity=100)
-    constr = light.object.constraints.new(type='COPY_TRANSFORMS')
-    constr.target = cameras[closest_camera_idx].object
+    if not use_color:
+        light = bsyn.Light.create('SPOT', intensity=200)
+        constr = light.object.constraints.new(type='COPY_TRANSFORMS')
+        constr.target = cameras[closest_camera_idx].object
 
-    light.object.data.spot_size = 1.22 # ~70 degrees
+        light.object.data.spot_size = 1.22 # ~70 degrees
 
-    comp.render(camera=cameras)
+    render_result = comp.render(camera=cameras)
 
     # Move to output directories.
     for view in views:
-        src_path = os.path.join(tmp_output_dir, f"{view.key}_mesh.png")
-        dst_path = os.path.join(input_directory, view.key, "mesh.png")
+        src_path = render_result.get_render_path('mesh', view.key)
+        dst_path = os.path.join(input_directory, view.key, f"{out_key}.png")
 
         # Load and place over the original image.
         original_image = (view.rgb * 255).astype(np.uint8)
@@ -107,6 +124,5 @@ def main(input_directory):
         cv2.imwrite(dst_path, cv2.cvtColor(new_image, cv2.COLOR_RGBA2BGRA))
 
 
-
 if __name__ == "__main__":
-    main(input_directory=args.input_directory)
+    main(input_directory=args.input_directory, use_color=args.use_color, out_key='mesh_color' if args.use_color else 'mesh_no_color')
